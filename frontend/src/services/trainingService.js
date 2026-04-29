0429c22a33e816aa973908630f90280c48f9da4a
Ahmadsaeed9490 <ahmadsaeed9490@gmail.com>
2026-04-29 14:58:12 +0500

    refactor: simplify resetWeights function in trainingService chore: add training_metrics.json to .gitignore

--- frontend/src/services/trainingService.js
+++ frontend/src/services/trainingService.js
@@ -1,7 +1,4 @@
 import { get, post } from './api';
 export const fetchTrainingStats = () => get("/training/stats");
 export const toggleTraining = () => post("/training/toggle", {});
-export const resetWeights = async () => {
-    const response = await post('/training/reset');
-    return response.data;
-};
+export const resetWeights = () => post('/training/reset', {});
